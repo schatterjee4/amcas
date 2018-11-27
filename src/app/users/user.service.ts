36e91417e65edfa79da9c6e7ae82f5edff6b2085
Your Name <sharmistha.chatterjee@cognizant.com>
2018-11-27 23:51:55 +0530

    all

---
+++ src/app/users/user.service.ts
@@ -0,0 +1,15 @@
+// user.service.ts
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class UserService {
+  constructor(private http: HttpClient) { }
+  url = 'http://localhost:4000';
+  getUsers() {
+    return this
+            .http
+            .get(`${this.url}/results`);
+        }
+}
